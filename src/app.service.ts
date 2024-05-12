cfcf1a6a32eae71147dad7779fe6d80cd76a8cd7
Igor Slotin <igorslotin@gmail.com>
2024-05-13 00:52:11 +0200

    Added link to the repo on the main page

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,7 +7,7 @@ export class AppService {
       <section style="display: flex; flex-direction: column; justify-content:center; align-items: center; height: 100%;">
         <a style="margin:10px 5px;" href="/customer/customer-transactions/1">Transactions for customer 1</a>
         <a style="margin:10px 5px;" href="/fraud/fraud-transactions/3">Fraud detection for customer 3</a>
-        <a style="margin:10px 5px;" href="/customer/customer-transactions/1">Link to Readme</a>
+        <a style="margin:10px 5px;" href="https://github.com/igor-slotin/seen-transactions">Link to the repo</a>
       </section>
     `
   }
